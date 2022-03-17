411d6c47261654c74ee12c31452e4b9e6b0c0b83
Timothy Huang <thuang87@berkeley.edu>
2022-03-17 04:35:03 -0700

    testing

--- index.js
+++ index.js
@@ -46,9 +46,7 @@ app.post("/todos", async (req, res) => {
 app.get("/todos", async (req, res) => {
   try {
     const allTodos = await pool.query("SELECT * FROM todo");
-    let text = res.text(allTodos.rows); // convert to plain text
-    console.log(text);
-    // res.json(allTodos.rows);
+    res.json(allTodos.rows);
   } catch (err) {
     console.error(err.message);
   }
