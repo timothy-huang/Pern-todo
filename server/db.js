fba21da068735abba3e886607bc7b35acfba4a02
Timothy Huang <thuang87@berkeley.edu>
2022-03-17 02:48:10 -0700

    connected to heroku hobby database

--- server/db.js
+++ server/db.js
@@ -1,11 +1,14 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "@hYN@!^56r4FSE",
-  host: "localhost",
+  user: "zrutfsrmursvkm",
+  password: "b949ff6dad4f30a790de83df698d3d6c78c1cfec4feb4c6836781e45ba5cbdf1",
+  host: "ec2-44-192-245-97.compute-1.amazonaws.com",
   port: 5432,
-  database: "perntodo"
+  database: "dd3rvrgeopsv7o",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
