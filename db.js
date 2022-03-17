22862b9da8c66ac4c14de402b9f9fda326950aaa
Timothy Huang <thuang87@berkeley.edu>
2022-03-17 05:52:07 -0700

    removed sensitive info :D

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "zrutfsrmursvkm",
-  password: "b949ff6dad4f30a790de83df698d3d6c78c1cfec4feb4c6836781e45ba5cbdf1",
-  host: "ec2-44-192-245-97.compute-1.amazonaws.com",
-  port: 5432,
-  database: "dd3rvrgeopsv7o",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
