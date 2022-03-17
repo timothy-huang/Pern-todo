0be76d457871365aad261053b9bfe71f4cf6307c
Timothy Huang <thuang87@berkeley.edu>
2022-03-17 03:31:40 -0700

    updated pathname

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); // req.body
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "./client/build")));
   app.get("*", (req, res) => {
-    res.sendfile(path.join((__dirname = "client/build/index.html")));
+    res.sendfile(path.join((__dirname = "./client/build/index.html")));
   });
 }
 
