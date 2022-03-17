b6c63f67b7f6ae5c0c434e1ff0ffe875227f9d31
Timothy Huang <thuang87@berkeley.edu>
2022-03-17 04:15:57 -0700

    changed which react files are served

--- index.js
+++ index.js
@@ -9,17 +9,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); // req.body
 
-// app.use(express.static(path.join(__dirname, "client/build")));
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "client/build")));
-//   app.get("*", (req, res) => {
-//     res.sendfile(path.join((__dirname = "client/build/index.html")));
-//   });
-// }
+app.use(express.static(path.join(__dirname, "client/build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/client/public/index.html"));
-// });
+if (process.env.NODE_ENV === "production") {
+  //production mode
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "client/build/index.html")));
+  });
+} else {
+  //build mode
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/public/index.html"));
+  });
+}
 
 //ROUTES//
 
