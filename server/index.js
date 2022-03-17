0eb9fe63b950f0c91434fdbc6df8996cf9f061e6
Timothy Huang <thuang87@berkeley.edu>
2022-03-17 03:17:37 -0700

    prepare to deploy

--- server/index.js
+++ server/index.js
@@ -2,10 +2,23 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json()); // req.body
+
+app.use(express.static(path.join(__dirname, "client/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "client/build/index.html")));
+  });
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+});
 
 //ROUTES//
 
@@ -80,6 +93,6 @@ app.delete("/todos/:id", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server has started on port 5000");
+app.listen(port, (req, res) => {
+  console.log(`server listening on port: ${port}`);
 });
